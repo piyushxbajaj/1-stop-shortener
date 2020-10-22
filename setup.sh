42cb797536e845a4115f040a28970f01ddf7458e
piyushxbajaj <piyushpiyush41@gmail.com>
2020-10-22 17:43:21 +0530

    final ig

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
-mkdir -p ~/.streamlit/
-echo "\[general]\n\email = \"piyushpiyush41@gmail.com\"\n\" > ~/.streamlit/credentials.toml echo "\[server]\n\
+echo “\
+[general]\n\
+email = \”piyushpiyush41@gmail.com\”\n\
+“ > ~/.streamlit/credentials.tomlecho “\
+[server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+“ > ~/.streamlit/config.toml
